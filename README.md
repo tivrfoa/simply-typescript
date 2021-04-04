c9d44f9f55a61e67f7f5b739aa8943b9cd37ff4b
Leandro Coutinho <lescoutinhovr@gmail.com>
2021-04-04 12:08:53 -0300

    Create package.json and set type to module
    Used yarn to create package.json and needed
    to change type to module to make node happy.

---
+++ README.md
@@ -0,0 +1,19 @@
+# app1
+
+
+### Compile
+
+`tsc`
+
+### Run
+
+`node output/main.js`
+
+### Useful Resources
+
+TypeScript Tutorial #14 - Modules
+
+https://www.youtube.com/watch?v=EpOPR03z4Vw
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import { add } from './util.js';
+
 function dividir(num1: number, num2: number): number{
     return num1 / num2;
 }
 
 
-console.log(dividir(7, 2))
+console.log(dividir(7, 2));
+console.log(add(7, 2));
